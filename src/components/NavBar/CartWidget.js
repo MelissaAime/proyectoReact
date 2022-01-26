a06f04e028998cded68071bd050c44ac976c2ef3
Melissa Aime <aimemelissa1@gmail.com>
2022-01-26 18:04:35 -0300

    Segunda clase 2

---
+++ src/components/NavBar/CartWidget.js
@@ -0,0 +1,5 @@
+export const CartWidget = () => {
+    return(
+        <p>Icono</p>
+    )
+}
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,10 +1,13 @@
 import './NavBar.css'
+import './CartWidget'
+import { CartWidget } from './CartWidget';
 
 export const NavBar = () => {
     return(
         <header className="header">
-            <h1>IMAGEN</h1>
-
+            <CartWidget/>
+            <h1>Nombre</h1>
+            
             <nav className="header-navbar">
                 <p className="header-item">Item 1</p>
                 <p className="header-item">Item 2</p>
