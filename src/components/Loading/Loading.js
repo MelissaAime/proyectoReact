54cb074dc759a95f95e543a0be305295424fee70
Melissa Aime <aimemelissa1@gmail.com>
2022-01-30 09:39:01 -0300

    agregado de productos, cambio loading por cargando

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -3,7 +3,7 @@ import { Spinner } from "react-bootstrap"
 export const Loading = () => {
     return (
         <div>
-            <h4>Loading...</h4>
+            <h4>Cargando productos...</h4>
 
             <Spinner animation="border" role="status">
                 <span className="visually-hidden">Loading...</span>
--- src/components/MyProducts/stock.js
+++ src/components/MyProducts/stock.js
@@ -4,20 +4,27 @@ export const stock = [
         id: 1,
         desc: 'Algo producto 1',
         price: 220,
-        image: 'https://via.placeholder.com/200'
+        image: 'https://img.icons8.com/nolan/96/image.png'
     },
     {
         name: 'Producto 2',
         id: 2,
         desc: 'Algo producto 2',
         price: 340,
-        image: 'https://via.placeholder.com/200'    
+        image: 'https://img.icons8.com/nolan/96/image.png'    
     },
     {
         name: 'Producto 3',
         id: 3,
         desc: 'Algo producto 3',
         price: 300,
-        image: 'https://via.placeholder.com/200'    
+        image: 'https://img.icons8.com/nolan/96/image.png'    
+    },
+    {
+        name: 'Producto 4',
+        id: 4,
+        desc: 'Algo producto 4',
+        price: 540,
+        image: 'https://img.icons8.com/nolan/96/image.png'    
     }    
 ]
